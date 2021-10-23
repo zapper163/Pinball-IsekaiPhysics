d499c9ddab443d6efd2f23c4df62584c1b2580da
Ruboki <ruboki.reuchi@gmail.com>
2021-10-23 09:11:40 +0200

    Update Globals.h

--- Pinball-IsekaiPhysics/Globals.h
+++ Pinball-IsekaiPhysics/Globals.h
@@ -21,8 +21,8 @@ enum update_status
 };
 
 // Configuration -----------
-#define SCREEN_WIDTH 254
-#define SCREEN_HEIGHT 470
+#define SCREEN_WIDTH 1016
+#define SCREEN_HEIGHT 1880
 #define SCREEN_SIZE 1
 #define WIN_FULLSCREEN false
 #define WIN_RESIZABLE false
